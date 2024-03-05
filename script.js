7adb27362ce54f36965ae60bcf178dd35402c501
guilhermedcf <guilhermedocarmoferreira@gmail.com>
2024-03-04 22:52:18 -0300

    feature

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const inputName = document.querySelector("#name");
 const inputSurname = document.querySelector("#surname");
 const inputCpf = document.querySelector("#cpf");
@@ -11,60 +10,67 @@ const inputCity = document.querySelector("#city");
 const inputState = document.querySelector("#state");
 const form = document.querySelector("#form");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-   
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
 
-    dataLocalStorage()
-    
+  dataLocalStorage();
 
-   });
+  form.reset();
+});
 
-  function validadateCpf(cpf) {
-    cpf = cpf.replace(/\D/g, ''); 
-    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
-    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
-    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); 
-    return cpf;
+function validadateCpf(cpf) {
+  cpf = cpf.replace(/\D/g, "");
+  cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+  cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+  cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  return cpf;
 }
 function formattingCpf() {
-    let input = document.querySelector('#cpf');
-    let newValue = input.value;
-    newValue = validadateCpf(newValue);
-    input.value = newValue;
+  let input = document.querySelector("#cpf");
+  let newValue = input.value;
+  newValue = validadateCpf(newValue);
+  input.value = newValue;
 }
-document.querySelector('#cpf').addEventListener('input', formattingCpf);
-
+document.querySelector("#cpf").addEventListener("input", formattingCpf);
 
 function ValidateCep(cep) {
-    cep = cep.replace(/\D/g, ''); 
-    cep = cep.replace(/(\d{5})(\d)/, '$1-$2'); 
-    return cep;
+  cep = cep.replace(/\D/g, "");
+  cep = cep.replace(/(\d{5})(\d)/, "$1-$2");
+  return cep;
 }
 
 function formattingCep() {
-    let input = document.querySelector('#cep');
-    let selectedValue = input.value;
-    selectedValue = ValidateCep(selectedValue);
-    input.value = selectedValue;
+  let input = document.querySelector("#cep");
+  let selectedValue = input.value;
+  selectedValue = ValidateCep(selectedValue);
+  input.value = selectedValue;
 }
-document.querySelector('#cep').addEventListener('input', formattingCep);
+document.querySelector("#cep").addEventListener("input", formattingCep);
 
 function desativeButton() {
-    const inputName = document.querySelector("#name").value;
-    const inputSurname = document.querySelector("#surname").value;
-    const inputCpf = document.querySelector("#cpf").value;
-    const inputData = document.querySelector("#date").value;
-    const inputCep = document.querySelector("#cep").value;
-    const inputAddress = document.querySelector("#address").value;
-    const inputNumber = document.querySelector("#number").value;
-    const inputComplement = document.querySelector("#complement").value;
-    const inputCity = document.querySelector("#city").value;
-    const inputState = document.querySelector("#state").value;
+  const inputName = document.querySelector("#name").value;
+  const inputSurname = document.querySelector("#surname").value;
+  const inputCpf = document.querySelector("#cpf").value;
+  const inputData = document.querySelector("#date").value;
+  const inputCep = document.querySelector("#cep").value;
+  const inputAddress = document.querySelector("#address").value;
+  const inputNumber = document.querySelector("#number").value;
+  const inputComplement = document.querySelector("#complement").value;
+  const inputCity = document.querySelector("#city").value;
+  const inputState = document.querySelector("#state").value;
 
-    if (inputName && inputSurname && inputCpf && inputData && inputCep && inputAddress && 
-        inputNumber && inputComplement && inputCity && inputState) {
+  if (
+    inputName &&
+    inputSurname &&
+    inputCpf &&
+    inputData &&
+    inputCep &&
+    inputAddress &&
+    inputNumber &&
+    inputComplement &&
+    inputCity &&
+    inputState
+  ) {
     document.querySelector("#login").disabled = false;
   } else {
     document.querySelector("#login").disabled = true;
@@ -72,35 +78,29 @@ function desativeButton() {
 }
 
 function dataLocalStorage() {
-const dataInput = {
-      name: inputName.value,
-      Surname: inputSurname.value,
-      CPF: inputCpf.value,
-      cep: inputCep.value,
-      Address: inputAddress.value,
-      Number: inputNumber.value,
-      Complement: inputComplement.value,
-      city: inputCity.value,
-      state: inputState.value,
-    };
+  const dataInput = {
+    name: inputName.value,
+    Surname: inputSurname.value,
+    CPF: inputCpf.value,
+    cep: inputCep.value,
+    Address: inputAddress.value,
+    Number: inputNumber.value,
+    Complement: inputComplement.value,
+    city: inputCity.value,
+    state: inputState.value,
+  };
 
-    
+  localStorage.setItem("dataInput", JSON.stringify(dataInput));
+  setTimeout(function () {
+    location.reload;
+  }, 3000);
+}
 
-    localStorage.setItem("dataInput", JSON.stringify(dataInput));
-    setTimeout(function (){
-      location.reload
-    },3000)
-  }
-
- 
-  function alert(){
-    Swal.fire({
-      title: "Seu formulário foi enviado!",
-      icon: "success",
-      showConfirmButton: false,
-      timer: 1500
-    });
-  }
-  
-
-  
+function alert() {
+  Swal.fire({
+    title: "Seu formulário foi enviado!",
+    icon: "success",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+}
